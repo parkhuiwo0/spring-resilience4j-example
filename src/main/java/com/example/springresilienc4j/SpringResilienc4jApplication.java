3b2566f0ed76d4ee21a9ba62ffda147278d67e2e
parkhuiwo0 <huiwo0@naver.com>
2024-03-21 21:27:43 +0900

    init

---
+++ src/main/java/com/example/springresilienc4j/SpringResilienc4jApplication.java
@@ -0,0 +1,13 @@
+package com.example.springresilienc4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResilienc4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringResilienc4jApplication.class, args);
+    }
+
+}
